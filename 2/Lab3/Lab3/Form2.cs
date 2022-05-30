216ffedd71ac452001a1aa8e0719fd0f9e0b322d
V-lad-K <krugonovskiy@gmail.com>
2022-05-30 13:46:19 +0300

    Test
    Test

---
+++ 2/Lab3/Lab3/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //.DataSource = Datab.data_dt;
+            //dataGridView1.DataSource = Datab.data_cl;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Datab.data_dt;
+
+            dataGridView1.DataSource = Datab.dt;
+            dataGridView1.DataSource = Datab.data_cl;
+        }
+    }
+}
