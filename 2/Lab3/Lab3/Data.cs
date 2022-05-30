768767b13692f39689517fe934dc64ac15cb2725
V-lad-K <krugonovskiy@gmail.com>
2022-05-30 13:43:01 +0300

    Test
    Test

---
+++ 2/Lab3/Lab3/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Lab3
+{
+    internal class Datab
+    {
+        public static DataGridView data_dt = new DataGridView();
+        public static DataGridViewColumn data_cl = new DataGridViewColumn();
+        public static DataTable dt = new DataTable();
+    }
+}
