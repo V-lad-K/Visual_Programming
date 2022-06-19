2f3ca3197414c1d1f649860c6b4a3d28d707952b
V-lad-K <84697726+V-lad-K@users.noreply.github.com>
2022-06-19 23:10:45 +0300

    lab2
    lab2

---
+++ 2/schedule/schedule/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace schedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(12, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "y(x)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(339, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 175);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "tab_count";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "b";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Lines;
+            chartArea3.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Lines;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 247);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(776, 160);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.Location = new System.Drawing.Point(139, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 41);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "x(t)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button4.Location = new System.Drawing.Point(403, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 41);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "y(x) параметрично";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button5.Location = new System.Drawing.Point(279, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 41);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "y(t)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "min_func";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(522, 425);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "max_func";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "A";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 135);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(821, 497);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ 2/schedule/schedule/Form1.cs
@@ -0,0 +1,380 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace schedule
+{
+    public partial class Form1 : Form
+    {
+        public double parametr_a = 0;
+        public Form1()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void cartesianChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            chart1.Series[0].Points.Clear();
+
+            double h = (double.Parse(textBox3.Text) - double.Parse(textBox2.Text) )/double.Parse(textBox1.Text);
+            for (int i = 1; i < Int32.Parse(textBox1.Text); i++)
+            {
+                users.Add(new User(i, double.Parse(textBox2.Text)+i*h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("y", typeof(double));
+            dt.Columns.Add("x", typeof(double));
+
+            double min;
+            double max;
+            min = users[0].func(users[0].x);
+            max = users[0].func(users[0].x);
+            for (int i = 0; i < users.Count; i++)
+            {
+                if(users[i].func(users[i].x)<min)
+                {
+                    min = users[i].func(users[i].x);
+                }
+                if (users[i].func(users[i].x) > max)
+                {
+                    max = users[i].func(users[i].x);
+                }
+                dt.Rows.Add(users[i].count, users[i].func(users[i].x), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func(users[i].x));
+            }
+
+            dataGridView1.DataSource = dt;
+
+            label1.Text = min.ToString();
+            label5.Text = max.ToString();
+        }
+        
+        public class User{
+            public int count;
+            public double x;
+            public User(int _count, double _x)
+            {
+                count = _count;
+                x = _x;
+            }
+            ~User()
+            {
+                
+            }
+            public double func(double x)
+            {
+                return (Math.Pow(x+2, 2.0/3.0) - Math.Pow(x-2, 2.0/3.0));
+            }
+            
+            public double func_x_t(double x, double parametr_a)
+            {
+                return (parametr_a / (Math.Pow(Math.Cos(x), 3.0))); 
+            }
+            public double func_y_t(double x, double parametr_a)
+            {
+                return parametr_a * Math.Pow(Math.Tan(x), 3);
+            }
+            public double func_y_x(double x, double parametr_a)
+            {
+                return parametr_a * Math.Pow(Math.Tan(Math.Acos(Math.Pow(parametr_a / x, 1.0 / 3.0))), 3.0);
+            }
+
+
+        }
+        List<User> users = new List<User>();
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void check(string value)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(value, "[^-0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                value = value.Remove(value.Length - 1);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            check(textBox1.Text);
+            
+        }
+
+        private void cartesianChart1_ChildChanged_1(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void btn_func(string value3, string value2, string value1)
+        {
+            double h = (double.Parse(value3) - double.Parse(value2)) / double.Parse(value1);
+            for (int i = 1; i < Int32.Parse(value1); i++)
+            {
+                users.Add(new User(i, double.Parse(value2) + i * h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("y", typeof(double));
+            dt.Columns.Add("t", typeof(double));
+
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                dt.Rows.Add(users[i].count, users[i].func_y_t(users[i].x, parametr_a), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func_y_t(users[i].x, parametr_a));
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            chart1.Series["Series1"].Points.Clear();
+            
+            
+            double h = (double.Parse(textBox3.Text) - double.Parse(textBox2.Text)) / double.Parse(textBox1.Text);
+            for (int i = 1; i < Int32.Parse(textBox1.Text); i++)
+            {
+                users.Add(new User(i, double.Parse(textBox2.Text) + i * h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("y", typeof(double));
+            dt.Columns.Add("t", typeof(double));
+
+            double min;
+            double max;
+            min = users[0].func_y_t(users[0].x, parametr_a);
+            max = users[0].func_y_t(users[0].x, parametr_a);
+            for (int i = users.Count-int.Parse(textBox1.Text)+1; i < users.Count; i++)
+            {
+                if (users[i].func_y_t(users[i].x, parametr_a) < min)
+                {
+                    min = users[i].func_y_t(users[i].x, parametr_a);
+                }
+                if (users[i].func_y_t(users[i].x, parametr_a) > max)
+                {
+                    max = users[i].func_y_t(users[i].x, parametr_a);
+                }
+                dt.Rows.Add(users[i].count, users[i].func_y_t(users[i].x, parametr_a), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func_y_t(users[i].x, parametr_a));
+            }
+
+            dataGridView1.DataSource = dt;
+
+
+            label1.Text = min.ToString();
+            label5.Text = max.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clear();
+            parametr_a = double.Parse(textBox4.Text);
+
+            chart1.Series[0].Points.Clear();
+            double h = (double.Parse(textBox3.Text) - double.Parse(textBox2.Text)) / double.Parse(textBox1.Text);
+            for (int i = 1; i < Int32.Parse(textBox1.Text); i++)
+            {
+                users.Add(new User(i, double.Parse(textBox2.Text) + i * h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("x", typeof(double));
+            dt.Columns.Add("t", typeof(double));
+
+            double min;
+            double max;
+            min = users[0].func_x_t(users[0].x, parametr_a);
+            max = users[0].func_x_t(users[0].x, parametr_a);
+            for (int i = users.Count - int.Parse(textBox1.Text) + 1; i < users.Count; i++)
+            {
+                if (users[i].func_x_t(users[i].x, parametr_a) < min)
+                {
+                    min = users[i].func_x_t(users[i].x, parametr_a);
+                }
+                if (users[i].func_x_t(users[i].x, parametr_a) > max)
+                {
+                    max = users[i].func_x_t(users[i].x, parametr_a);
+                }
+                dt.Rows.Add(users[i].count, users[i].func_x_t(users[i].x, parametr_a), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func_x_t(users[i].x, parametr_a));
+            }
+
+            dataGridView1.DataSource = dt;
+
+            label1.Text = min.ToString();
+            label5.Text = max.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            chart1.Series[0].Points.Clear();
+            double h = (double.Parse(textBox3.Text) - double.Parse(textBox2.Text)) / double.Parse(textBox1.Text);
+            for (int i = 1; i < Int32.Parse(textBox1.Text); i++)
+            {
+                users.Add(new User(i, double.Parse(textBox2.Text) + i * h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("y", typeof(double));
+            dt.Columns.Add("x", typeof(double));
+
+
+            double min;
+            double max;
+            min = users[0].func_y_x(users[0].x, parametr_a);
+            max = users[0].func_y_x(users[0].x, parametr_a);
+            for (int i = users.Count - int.Parse(textBox1.Text) + 1; i < users.Count; i++)
+            {
+                if (users[i].func_y_x(users[i].x, parametr_a) < min)
+                {
+                    min = users[i].func_y_x(users[i].x, parametr_a);
+                }
+                if (users[i].func_y_x(users[i].x, parametr_a) > max)
+                {
+                    max = users[i].func_y_x(users[i].x, parametr_a);
+                }
+                dt.Rows.Add(users[i].count, users[i].func_y_x(users[i].x, parametr_a), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func_y_x(users[i].x, parametr_a));
+            }
+
+            dataGridView1.DataSource = dt;
+
+            label1.Text = min.ToString();
+            label5.Text = max.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            parametr_a = double.Parse(textBox4.Text);
+
+            Clear();
+
+            double h = (double.Parse(textBox3.Text) - double.Parse(textBox2.Text)) / double.Parse(textBox1.Text);
+            for (int i = 1; i < Int32.Parse(textBox1.Text); i++)
+            {
+                users.Add(new User(i, double.Parse(textBox2.Text) + i * h));
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("номер", typeof(int));
+            dt.Columns.Add("x", typeof(double));
+            dt.Columns.Add("y", typeof(double));
+
+
+            double min;
+            double max;
+            min = users[0].func_y_t(users[0].x, parametr_a);
+            max = users[0].func_y_t(users[0].x, parametr_a);
+            for (int i = users.Count - int.Parse(textBox1.Text) + 1; i < users.Count; i++)
+            {
+                if (users[i].func_y_t(users[i].x, parametr_a) < min)
+                {
+                    min = users[i].func_y_t(users[i].x, parametr_a);
+                }
+                if (users[i].func_y_t(users[i].x, parametr_a) > max)
+                {
+                    max = users[i].func_y_t(users[i].x, parametr_a);
+                }
+                dt.Rows.Add(users[i].count, users[i].func_y_t(users[i].x, parametr_a), users[i].x);
+                chart1.Series["Series1"].Points.AddXY(users[i].x, users[i].func_y_t(users[i].x, parametr_a));
+            }
+
+            dataGridView1.DataSource = dt;
+
+            label1.Text = min.ToString();
+            label5.Text = max.ToString();
+        }
+
+        public void empty_check()
+        {
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0)
+            {
+                MessageBox.Show("parametrs should be not empty");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            check(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            check(textBox3.Text);
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+        void Clear()
+        {
+            if (this.dataGridView1.DataSource != null)
+            {
+                this.dataGridView1.DataSource = null;
+            }
+            else
+            {
+                this.dataGridView1.Rows.Clear();
+            }
+            dataGridView1.Rows.Clear();
+            chart1.Series["Series1"].Points.Clear();
+            chart1.Series[0].Points.Clear();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
